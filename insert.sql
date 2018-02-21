24fa9d5541947cc763030c69b8cad4cd8bbdbe53
Chady Kamar <chady.kamar@gmail.com>
2018-02-21 16:32:34 -0500

    Sample inserts and selects

---
+++ insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO users
+VALUES(01, 'John Doe', 'john.doe@email.com', '123123', '123 1st ave', '0000000', '01/01/1950');
+
+INSERT INTO users
+VALUES(02, 'Jane Doe', 'jane.doe@email.com', '12341234', '123 1st ave', '0000000', '01/01/1955');
+
+INSERT INTO users
+VALUES(03, 'John Smith', 'john.smith@email.com', '123123', '456 2nd ave', '1111111', '01/01/1970');
+
+INSERT INTO users
+VALUES(04, 'Jane Smith', 'jane.smith@email.com', 'password', '456 2nd ave', '11111111', '01/01/1980');
